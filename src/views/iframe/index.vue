b79bfd6d07c46fa381d9e3eda62d02cdc9f3a30b
devtint <devtint@gmail.com>
2022-08-03 17:42:30 +0800

    fix: 关闭按钮固定/dialog全屏展示

--- src/views/iframe/index.vue
+++ src/views/iframe/index.vue
@@ -1,12 +1,19 @@
 <template>
   <div class="iframe-container">
+    <div class="header" v-if="dialogVisible">
+      <div class="title">{{ title }}</div>
+      <div class="close">
+        <el-button type="info" plain circle icon="el-icon-close" @click="handleClose"></el-button>
+      </div>
+    </div>
     <el-dialog
-      :title="title"
       :visible.sync="dialogVisible"
       width="100%"
       :modal-append-to-body="false"
       :modal="false"
       top="0"
+      fullscreen
+      :show-close="false"
       destroy-on-close
       :before-close="handleClose"
     >
@@ -81,4 +88,23 @@ export default {
 // ::v-deep .v-modal {
 //   position: absolute !important;
 // }
+.header {
+  // 固定在顶部
+  position: fixed;
+  top: 50px;
+  // position: relative;
+  width: 80%;
+  height: 50px;
+  background-color: #fff;
+  padding: 0 20px;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  z-index: 3000;
+
+  .title {
+    font-size: 18px;
+    font-weight: bold;
+  }
+}
 </style>
