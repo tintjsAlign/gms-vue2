1621ec60db3762918220305dd40d40b4430a6422
devtint <devtint@gmail.com>
2022-08-04 17:48:07 +0800

    style: 修改样式

--- src/components/ShowFileContent/index.vue
+++ src/components/ShowFileContent/index.vue
@@ -8,11 +8,15 @@
           <el-image style="width: 100%" :src="fileUrl" fit="cover"></el-image>
         </div>
         <div v-else-if="fileType === 'doc'">
-          <div v-html="fileContent"></div>
+          <el-alert :closable="false" :title="title" show-icon>
+          <el-alert :closable="false">
+            <div v-html="fileContent"></div>
+          </el-alert>
         </div>
         <div v-else>
-          <!-- <div>{{ fileContent }}</div> -->
           <el-alert :closable="false" :title="title" :type="alertType" show-icon>
+          </el-alert>
+          <el-alert :closable="false" :type="alertType">
             <div v-html="fileContent"></div>
           </el-alert>
         </div>
@@ -25,6 +29,7 @@
 export default {
   name: '',
   components: {},
+  inject: ['reload'],
   props: {},
   data() {
     return {
@@ -43,7 +48,7 @@ export default {
       // 根据文件后缀判断展示图片还是文件或者文本
       if (res.match(/\.(jpg|jpeg|png|gif|bmp|svg)$/)) {
         this.fileType = 'image'
-        this.fileUrl = res
+        this.fileUrl = process.env.VUE_APP_BASE_API + '/' + res
       } else if (res.match(/\.(txt|doc|docx|xls|xlsx|ppt|pptx|pdf)$/)) {
         this.fileType = 'doc'
         this.fileContent = res
@@ -53,13 +58,17 @@ export default {
         // 分行
         this.fileContent = this.fileContent.replace(/\n/g, '<br>')
         // 控制alertType
-        if (res.match(/成功/g)) {
+        if (res.match(/成功::/g)) {
           this.alertType = 'success'
         } else {
-          this.alertType = 'warning'
+          this.alertType = 'info'
         }
       }
       this.drawer = true
+    },
+    handleClose(done) {
+      this.reload()
+      done()
     }
   }
 }
