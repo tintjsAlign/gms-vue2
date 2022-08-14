1c54a85674c5984663c91a3a6aaef1f04d9926bd
devtint <devtint@gmail.com>
2022-08-14 17:44:30 +0800

    feat: 更换logo

--- src/layout/components/Sidebar/Logo.vue
+++ src/layout/components/Sidebar/Logo.vue
@@ -30,7 +30,8 @@ export default {
   data() {
     return {
       title: '密评工具平台',
-      logo: 'https://wpimg.wallstcn.com/69a1c46c-eb1c-4b46-8bd4-e9e686ef5251.png'
+      // logo: 'https://wpimg.wallstcn.com/69a1c46c-eb1c-4b46-8bd4-e9e686ef5251.png'
+      logo: require('@/icons/logo.png')
     }
   }
 }
@@ -51,7 +52,7 @@ export default {
   width: 100%;
   height: 50px;
   line-height: 50px;
-  background: #2b2f3a;
+  // background: #2b2f3a;
   text-align: center;
   overflow: hidden;
 
